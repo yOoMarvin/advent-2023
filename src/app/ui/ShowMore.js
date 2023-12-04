628dc818e4c81b3c09473828e892350829dce113
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2023-12-04 14:20:11 +0000

    day 02 of react holiday with refs

--- src/app/ui/ShowMore.js
+++ src/app/ui/ShowMore.js
@@ -1,20 +1,33 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const ShowMore = ({ content, maxChar }) => {
   const [expanded, setExpanded] = useState(false);
+  const contentRef = useRef(null);
 
   const toggleExpanded = () => {
     setExpanded(!expanded);
   };
 
+  const logScrollHeight = () => {
+    console.log(contentRef.current.scrollHeight);
+  };
+
   return (
-    <div className="w-full rounded-lg border border-neutral-100 bg-neutral-50 p-4">
+    <div
+      className={`w-full rounded-lg border border-neutral-100 bg-neutral-50 p-4 max-h-${
+        expanded ? "none" : "100"
+      } overflow-hidden`}
+      ref={contentRef}
+    >
       <p>{expanded ? content : content.slice(0, maxChar) + " ..."}</p>
       {content.length > maxChar && (
         <button
           className="transition__base mt-4 rounded-md bg-blue-100 px-3 py-2 font-medium text-blue-600 shadow-sm hover:bg-blue-200 hover:text-blue-700"
-          onClick={toggleExpanded}
+          onClick={() => {
+            toggleExpanded();
+            logScrollHeight();
+          }}
         >
           {expanded ? "Show Less" : "Show More"}
         </button>
